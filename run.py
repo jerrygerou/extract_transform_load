28526a5612fd5ce975804d848996a83fa6d0607d
Jerry Gerou <jerrysgerou@gmail.com>
2018-11-24 12:41:56 -0600

    Update readme

--- run.py
+++ run.py
@@ -1,30 +1,31 @@
 import json
 from collections import defaultdict
-from itertools import chain
 
+# Known Rules
+    # Any value is more specific than wildcard
+    # A match on multiple criteria is more specific than a match on one
+    # For a 'tie', publisher (most specific), duration (second specific), price (least)
 
-# Any value is more specific than wildcard
-# A match on multiple criteria is more specific than a match on one
-# For a 'tie', publisher (most specific), duration (second specific)
-
-
-# Order of importance logic:
+# Purchase of importance logic:
     # 1) Publisher, Price, Duration
     # 2) Publisher, *, Duration
     # 3) Publisher, Price, *
-    # 4) *, *, Duration
-    # 5) *, Price, Duration
-    # 6) *, *, *
+    # 4) *, Price, Duration
+    # 5) *, *, Duration
+    # 6) *, Price, *
+    # 7) *, *, *
 
-def get_orders():
-    order_file = open('purchase_data.csv')
-    orders = []
-    for line in order_file:
+def get_purchases():
+    # Get list of purchases
+    purchase_file = open('purchase_data.csv')
+    purchases = []
+    for line in purchase_file:
         each_part = line.split(',')
-        orders.append([i.replace('\n', '') for i in each_part])
-    return orders
+        purchases.append([i.replace('\n', '') for i in each_part])
+    return purchases
 
 def get_buckets():
+    # Get list of buckets
     bucket_file = open('purchase_buckets.csv')
     buckets = []
     for line in bucket_file:
@@ -33,76 +34,89 @@ def get_buckets():
     return buckets
 
 
-def bucket_all_items(orders, buckets):
+def bucket_all_items(purchases, buckets):
+    # Create empty dictionary to hold resulted buckets
     buckets_dict = defaultdict(list)
+    # Create empty list to hold orders to prevent duplicate entries in duplicated buckets
     added_to_a_bucket = []
 
     for bucket in buckets:
         buckets_dict[bucket[0], bucket[1], bucket[2]] = []
 
-    for order in orders:
+    for purchase in purchases:
         # publisher = [2], price = [4], duration = [5]
-        publisher = order[2]
-        price = order[4]
-        duration = order[5]
-        key_to_find = publisher, price, duration
+        publisher = purchase[2]
+        price = purchase[4]
+        duration = purchase[5]
+        bucket_to_find = publisher, price, duration
         # 1) Publisher, Price, Duration
         for k, v in buckets_dict.items():
-            if key_to_find[0].lower() == k[0].lower() and key_to_find[1] == k[1] and key_to_find[2] == k[2]:
-                if order not in added_to_a_bucket:
-                    ','.join(order)
-                    buckets_dict[k[0], k[1], k[2]].append(','.join(order))
-                    added_to_a_bucket.append(order)
+            if bucket_to_find[0].lower() == k[0].lower() and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                if purchase not in added_to_a_bucket:
+                    ','.join(purchase)
+                    buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                    added_to_a_bucket.append(purchase)
         else:
             # 2) Publisher, *, Duration
-            key_to_find = publisher, '*', duration
+            bucket_to_find = publisher, '*', duration
             for k, v in buckets_dict.items():
-                if key_to_find[0].lower() == k[0].lower() and key_to_find[1] == k[1] and key_to_find[2] == k[2]:
-                    if order not in added_to_a_bucket:
-                        buckets_dict[k[0], k[1], k[2]].append(','.join(order))
-                        added_to_a_bucket.append(order)
+                if bucket_to_find[0].lower() == k[0].lower() and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                    if purchase not in added_to_a_bucket:
+                        buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                        added_to_a_bucket.append(purchase)
             else:
                 # 3) Publisher, Price, *
-                key_to_find = publisher, price, '*'
+                bucket_to_find = publisher, price, '*'
                 for k, v in buckets_dict.items():
-                    if key_to_find[0].lower() == k[0].lower() and key_to_find[1] == k[1] and key_to_find[2] == k[2]:
-                        if order not in added_to_a_bucket:
-                            buckets_dict[k[0], k[1], k[2]].append(','.join(order))
-                            added_to_a_bucket.append(order)
+                    if bucket_to_find[0].lower() == k[0].lower() and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                        if purchase not in added_to_a_bucket:
+                            buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                            added_to_a_bucket.append(purchase)
                 else:
-                    # 4) *, *, Duration
-                    key_to_find = '*', '*', duration
+                    # 4) *, Price, Duration
+                    bucket_to_find = '*', price, duration
                     for k, v in buckets_dict.items():
-                        if key_to_find[0] == k[0] and key_to_find[1] == k[1] and key_to_find[2] == k[2]:
-                            if order not in added_to_a_bucket:
-                                buckets_dict[k[0], k[1], k[2]].append(','.join(order))
-                                added_to_a_bucket.append(order)
+                        if bucket_to_find[0] == k[0] and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                            if purchase not in added_to_a_bucket:
+                                buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                                added_to_a_bucket.append(purchase)
                     else:
-                        # 5) *, Price, Duration
-                        key_to_find = '*', price, duration
+                        # 5) *, *, Duration
+                        bucket_to_find = '*', '*', duration
                         for k, v in buckets_dict.items():
-                            if key_to_find[0] == k[0] and key_to_find[1] == k[1] and key_to_find[2] == k[2]:
-                                if order not in added_to_a_bucket:
-                                    buckets_dict[k[0], k[1], k[2]].append(','.join(order))
-                                    added_to_a_bucket.append(order)
+                            if bucket_to_find[0] == k[0] and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                                if purchase not in added_to_a_bucket:
+                                    buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                                    added_to_a_bucket.append(purchase)
+                        else:
+                            # 6) *, Price, *
+                            bucket_to_find = '*', price, '*'
+                            for k, v in buckets_dict.items():
+                                if bucket_to_find[0] == k[0] and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                                    if purchase not in added_to_a_bucket:
+                                        buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                                        added_to_a_bucket.append(purchase)
                             else:
-                                # 6) *, *, *
-                                key_to_find = '*', '*', '*'
+                                # 7) *, *, *
+                                bucket_to_find = '*', '*', '*'
                                 for k, v in buckets_dict.items():
-                                    if key_to_find[0] == k[0] and key_to_find[1] == k[1] and key_to_find[2] == k[2]:
-                                        if order not in added_to_a_bucket:
-                                            buckets_dict[k[0], k[1], k[2]].append(','.join(order))
-                                            added_to_a_bucket.append(order)
+                                    if bucket_to_find[0] == k[0] and bucket_to_find[1] == k[1] and bucket_to_find[2] == k[2]:
+                                        if purchase not in added_to_a_bucket:
+                                            buckets_dict[k[0], k[1], k[2]].append(','.join(purchase))
+                                            added_to_a_bucket.append(purchase)
+    return buckets_dict
 
+def generate_file(buckets_dict):
     # Create final list of bucketed purchases
     output_of_buckets = []
     for key, value in buckets_dict.items():
         output_of_buckets.append({'bucket':key,'purchases':value})
 
-    # Output a json file with correct separators
-    with open('result.json', 'w') as result_file:
+    # Output a json file
+    with open('json_results.json', 'w') as result_file:
         json.dump(output_of_buckets, result_file)
 
-orders = get_orders()
+purchases = get_purchases()
 buckets = get_buckets()
-bucket_all_items(orders, buckets)
+bucketed_purchases = bucket_all_items(purchases, buckets)
+generate_file(bucketed_purchases)
